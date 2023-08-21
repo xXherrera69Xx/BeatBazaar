da8eb84ec8532c22b21ef3e09f2fce77af60024c
Erik Herrera <106594931+xXherrera69Xx@users.noreply.github.com>
2023-08-21 20:20:48 -0300

    Delete BeatBazaar directory

--- BeatBazaar/config.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'BeatBazaar');
-if (!$conn){
-    die('Error de Conexion (' . mysqli_connect_errno() . ')' . mysqli_connect_error());
-    
-}
