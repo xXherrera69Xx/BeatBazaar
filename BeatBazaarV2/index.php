6152b441e15a0f1e1e80a2deba91a488b8ab3058
Erik Herrera <106594931+xXherrera69Xx@users.noreply.github.com>
2023-11-21 00:26:16 -0300

    Add files via upload

---
+++ BeatBazaarV2/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <script src="https://kit.fontawesome.com/c6765a010a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav>
+       <div class="icon">Beat<b style="color:aqua;">Bazaar</b></div>
+       <div class="search_box">
+        <input type="search" placeholder="Buscar">
+        <span class="fa fa-search"></span>
+       </div>
+       <ol>
+
+       <li><a href="#">Contacto</a></li>
+       <li><a href="#">Crear Cuenta</a></li>
+       <li><a href="#">Iniciar Sesion</a></li>
+       <li><a href="#">Contacto</a></li>
+
+       </ol>
+    </nav>
+      <div class="container">
+         <div class="slide">
+          <ul>
+             <li><img src="imagenes/carrusel/devil.jpg" alt=""></li>
+             <li><img src="imagenes/carrusel/uufo.jpg" alt=""></li>
+             <li><img src="imagenes/carrusel/c.jpg" alt=""></li>
+             <li><img src="imagenes/carrusel/guitarra.jpg" alt=""></li>
+
+
+
+          </ul>
+
+
+
+        </div>
+  
+
+      </div>
+
+
+</body>
+</html>
